b5eb2ae84ef57afa8cf80d5194315062b4d02deb
Van Dao <dao28901@gmail.com>
2025-02-18 15:49:22 +0700

    tao trang login #2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import "./App.scss";
 import Login from "./Components/Login/Login";
 import Nav from "./Components/Navigation/Nav";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
-    <Router>
-      <Nav />
-      <Switch>
-        <Route path="/home" exact>
-          HomePage
-        </Route>
-        <Route path="/user">User</Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="*">404 not found</Route>
-      </Switch>
-    </Router>
+    <div style={{ backgroundColor: "#f2f4f7", minHeight: "100vh" }}>
+      <Router>
+        <Nav />
+        <marquee>Welcome to my personal project (❁´◡`❁)</marquee>
+        <Switch>
+          <Route path="/home" exact>
+            HomePage
+          </Route>
+          <Route path="/user">User</Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">404 not found</Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,30 +2,34 @@ import React from "react";
 import "./Login.scss";
 const Login = () => {
   return (
-    <div className="container login-container p-5 mt-5">
-      <div className="left">
-        <div className="title">VanDao</div>
-        <div className="text">Personal Project</div>
-      </div>
-      <div className="right p-3">
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          className="form-control mt-2"
-          id="password"
-          placeholder="Password"
-        />
-        <button className="btn btn-primary mt-3">Log In</button>
-        <div className="mt-1 text-center">
-          <a href="#!">Forgot your password?</a>
+    <div className="container p-5 mt-5">
+      <div className="row login-container ">
+        <div className="left col-7 d-none d-md-block p-3">
+          <div className="title">VanDao</div>
+          <div className="text">Personal Project</div>
         </div>
-        <hr />
-        <button className="btn btn-success">Create new account</button>
+        <div className="right col-12 col-md-4 p-3">
+          <div className="title d-sm-none d-block text-center">VanDao</div>
+          {/*>=768:col-md-4:use,<=768 :col-12 */}
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            className="form-control mt-2"
+            id="password"
+            placeholder="Password"
+          />
+          <button className="btn btn-primary mt-3">Log In</button>
+          <div className="mt-1 text-center">
+            <a href="#!">Forgot your password?</a>
+          </div>
+          <hr />
+          <button className="btn btn-success">Create new account</button>
+        </div>
       </div>
     </div>
   );
