e045c5e438680cac56ee772862bedbdee441b54d
Van Dao <dao28901@gmail.com>
2025-02-18 16:08:34 +0700

    tao trang register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Login from "./Components/Login/Login";
 import Nav from "./Components/Navigation/Nav";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Register from "./Components/Register/register";
 function App() {
   return (
     <div style={{ backgroundColor: "#f2f4f7", minHeight: "100vh" }}>
       <Router>
         <Nav />
-        <marquee>Welcome to my personal project (❁´◡`❁)</marquee>
+        {/* <marquee>Welcome to my personal project (❁´◡`❁)</marquee> */}
         <Switch>
           <Route path="/home" exact>
             HomePage
@@ -14,6 +15,9 @@ function App() {
           <Route path="/user">User</Route>
           <Route path="/login">
             <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
           </Route>
           <Route path="*">404 not found</Route>
         </Switch>
--- src/Components/Register/register.js
+++ src/Components/Register/register.js
@@ -1,16 +1,16 @@
 import React from "react";
-import "./Login.scss";
+import "./register.scss";
 import { useHistory } from "react-router-dom";
-const Login = () => {
+const Register = () => {
   let history = useHistory();
-  const handleCreateNewAccount = () => {
-    history.push("/register");
+  const handleLogin = () => {
+    history.push("/login");
   };
   return (
     <div className="container p-5 mt-5">
       <div className="row login-container ">
         <div className="left col-7 d-none d-md-block p-3">
-          <div className="title">VanDao: login</div>
+          <div className="title">VanDao :register</div>
           <div className="text">Personal Project</div>
         </div>
         <div className="right col-12 col-md-4 p-3">
@@ -23,21 +23,33 @@ const Login = () => {
             placeholder="Email"
           />
           <input
+            type="number"
+            className="form-control mt-2"
+            id="PhoneNumber"
+            placeholder="PhoneNumber"
+          />
+          <input
+            type="text"
+            className="form-control mt-2"
+            id="username"
+            placeholder="username"
+          />
+          <input
             type="password"
             className="form-control mt-2"
             id="password"
             placeholder="Password"
           />
-          <button className="btn btn-primary mt-3">Log In</button>
-          <div className="mt-1 text-center">
-            <a href="#!">Forgot your password?</a>
-          </div>
+          <input
+            type="password"
+            className="form-control mt-2"
+            id="confirmPassword"
+            placeholder="confirmPassword"
+          />
+          <button className="btn btn-primary mt-3">Create account</button>
           <hr />
-          <button
-            className="btn btn-success"
-            onClick={() => handleCreateNewAccount()}
-          >
-            Create new account
+          <button className="btn btn-success" onClick={() => handleLogin()}>
+            Already have a account ? Login
           </button>
         </div>
       </div>
@@ -45,4 +57,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
